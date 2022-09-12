16c3c390307769865a61e0a821f21e6cd74fdc73
maybethee <jozalamun@gmail.com>
2022-09-11 20:31:56 -0400

    Add function to change colored pixels to be randomly generated RGB values

--- script.js
+++ script.js
@@ -1,4 +1,10 @@
 
+
+
+// i think i need a functino that caluclates the height and width values for each div that gets created in setCanvasPixels() below. I can know the size of the full canvas, whatever it is i decide it to be, and i get the desired amount of boxes through an input (it can be a prompt for now). but how do i convert that into pixel height/width? amount of pixels per side gives me the total number of divs..... and then just divide that by the canvas size? i think that could work, but how do i take borders into account? i guess i should just not have borders, but then idk how i'm gonna be able to tell whether it fills the canvas all the way?
+
+// ok so that works, now i just need to put that in a function, and call that function in the one below, and then..... change the style of the pixel base inside that function? i think that's right actually
+
 let canvas = document.querySelector('#canvasContainer')
 const rainbow = document.querySelector('#btnRainbow');
 
@@ -58,8 +64,16 @@ function setCanvasPixels() {
     const divs = document.createElement('div');
     canvas.appendChild(divs);
     divs.classList.add('pixel');
-  }
 
+    const setBg = () => {
+
+      const randomColor = Math.floor(Math.random()*16777215).toString(16);
+      div.style.backgroundColor = "#" + randomColor;
+      color.innerHTML = "#" + randomColor;
+      setBg();
+    }
+      
+  }
 }
 
 setCanvasPixels();
