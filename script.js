9dde52fca1e469745154e355f8656a6b09e22514
maybethee <jozalamun@gmail.com>
2022-09-11 21:57:05 -0400

    Fix makeRainbow function to activate on a button press

--- script.js
+++ script.js
@@ -1,11 +1,5 @@
 
-
-
-// i think i need a functino that caluclates the height and width values for each div that gets created in setCanvasPixels() below. I can know the size of the full canvas, whatever it is i decide it to be, and i get the desired amount of boxes through an input (it can be a prompt for now). but how do i convert that into pixel height/width? amount of pixels per side gives me the total number of divs..... and then just divide that by the canvas size? i think that could work, but how do i take borders into account? i guess i should just not have borders, but then idk how i'm gonna be able to tell whether it fills the canvas all the way?
-
-// ok so that works, now i just need to put that in a function, and call that function in the one below, and then..... change the style of the pixel base inside that function? i think that's right actually
-
-let canvas = document.querySelector('#canvasContainer')
+let canvas = document.querySelector('#canvasContainer');
 const rainbow = document.querySelector('#btnRainbow');
 
 
@@ -34,9 +28,6 @@ function isPositiveInteger(str) {
   return false;
 }
 
-
-
-
 // function that finds the required height/width size of interior divs
 function findDimensions(getCanvasArea) {
 
@@ -64,19 +55,33 @@ function setCanvasPixels() {
     const divs = document.createElement('div');
     canvas.appendChild(divs);
     divs.classList.add('pixel');
-
-    const setBg = () => {
-
-      const randomColor = Math.floor(Math.random()*16777215).toString(16);
-      div.style.backgroundColor = "#" + randomColor;
-      color.innerHTML = "#" + randomColor;
-      setBg();
-    }
-      
   }
 }
 
+// function to add rainbow option attached to button
+const makeRainbow = () => {
+
+  rainbow.addEventListener('click', function() {
+    
+    divs.forEach(div => {
+
+      div.addEventListener('mouseover', function() {
+        
+        // alter class for the new mouseover event to use
+        div.setAttribute('class', 'pixelRainbow');
+        const randomColor = Math.floor(Math.random()*16777215).toString(16);
+        div.style.backgroundColor = "#" + randomColor;
+      
+    })
+  })
+})
+}
+
+// sets pixels sized correctly according to prompt input
 setCanvasPixels();
+
+// rainbow function
+makeRainbow();
 
 const divs = document.querySelectorAll('.pixel');
 
@@ -85,7 +90,7 @@ divs.forEach(div => {
   div.style.width = pixelDimension + 'px';
   div.style.height = pixelDimension + 'px';
   
-  div.addEventListener('mouseover', (e) => {
+  div.addEventListener('mouseover', () => {
     div.setAttribute('class', 'pixelColor');
    
   })
